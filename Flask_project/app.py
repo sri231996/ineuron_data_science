5b1fc37462ce0a13fd7899ad777bc9907ed7af7a
sri231996 <67504502+sri231996@users.noreply.github.com>
2023-02-24 17:26:51 +0530

    Update app.py

--- Flask_project/app.py
+++ Flask_project/app.py
@@ -1,1 +1,27 @@
-This is a new Flask project
+from flask import Flask
+
+app = Flask(__name__)
+
+# In Flask, app = Flask(__name__) creates a new instance of the Flask class, which is the central class in a Flask application. The __name__ parameter is used to determine the name of the application's module or package.
+
+# When you create a Flask instance using Flask(__name__), Flask uses the __name__ parameter to locate the application's resources, such as templates and static files. By default, Flask assumes that these resources are located in a directory named templates and a directory named static that are located in the same directory as the application module or package.
+
+# Route (for handling any request)
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+
+
+if(__name__)=='__main__':
+    app.run(host="0.0.0.0",port=5000)
+
+
+# In Flask, app.run() is a method that starts a development server. The host and port parameters of the app.run() method specify the network interface and port number on which the Flask application should listen for incoming requests.
+
+# In the example you provided, host="0.0.0.0" specifies that the Flask application should listen on all network interfaces, which means it can be accessed from any IP address on the network. port=5000 specifies that the Flask application should listen on port number 5000.
+
+# So, when you run the Flask application with app.run(host="0.0.0.0",port=5000), the development server starts listening on port 5000 on all network interfaces. This means that if you access the Flask application from another device on the same network, you can use the IP address of the device running the Flask application and port 5000 to access the application. For example, if the IP address of the device running the Flask application is 192.168.1.100, you can access the application by visiting http://192.168.1.100:5000 in a web browser on another device on the same network.
+
+
+
